0b170915df883f0e468147d6e32178283120532f
Aqil Azmi <aqil@dhcp-10-250-4-89.harvard.edu>
2023-03-25 07:30:01 -0400

    my midterm outline

--- src/midterm/midterm.lean
+++ src/midterm/midterm.lean
@@ -13,21 +13,27 @@ class mygroup (α : Type*) :=
 (mul: α → α → α)
 (e: α)
 (inv: α → α)
-(mul_assoc : ∀ x y z : α, mul (mul x y) z = mul x (mul y z))
+(mul_assoc: ∀ x y z : α, mul (mul x y) z = mul x (mul y z))
 (mul_identity: ∀ x : α, mul x e = x)
 (identity_mul: ∀ x : α, mul e x = x)
+(elements: set α)
 
+--Then, we define a subgroup H of G (same group axioms as above with some additional properties)
+class subgroup (α : Type*) :=
+(mul: α → α → α)
+(e: α)
+(inv: α → α)
+(mul_assoc: ∀ x y z : α, mul (mul x y) z = mul x (mul y z))
+(mul_identity: ∀ x : α, mul x e = x)
+(identity_mul: ∀ x : α, mul e x = x)
+(elements: set α)
+axiom subgroup_subset {α : Type*} [mygroup α] (H : subgroup α) : H.elements ⊆ mygroup.elements
+axiom subgroup_nonempty {α : Type*} (H : subgroup α) : ∃ x : α, x ∈ H.elements
+axiom subgroup_closedproducts {α : Type*} (H : subgroup α) : ∀ x y : α, x ∈ H.elements → y ∈ H.elements → H.mul x y ∈ H.elements
 
---Then, we define a subgroup H of G
+--Then, we define the binary relation
 
-
-
-
---Then, we define the binary element
-
-
---Then, we prove that the binary element is an equivalence relation on G
-
+--Then, we prove that the binary relation is an equivalence relation on G
 
 --Then, we define the left coset of H
 
